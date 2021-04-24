a5a48e77496ec62052b6b5c7e00b9ce79fabf454
k_tihonov <k_tihonov@mail.ru>
2021-04-25 02:01:14 +0300

    add burger button

--- src/components/ArtistList.jsx
+++ src/components/ArtistList.jsx
@@ -7,40 +7,48 @@ import ArtistCard from "./ArtistCard";
 export default function ArtistList() {
   const [artist, setArtist] = useState(null);
   const [loading, setLoading] = useState(false);
+  const [search, setSearch] = useState("");
+  const [filteredArtists, setFilteredArtists] = useState([]);
 
-  const getList = async () => {
-    await axios
+  useEffect(() => {
+    setLoading(true);
+     axios
       .get("https://api.artic.edu/api/v1/artists")
       // Extract the DATA from the received response
       .then((res) => {
         setArtist(res.data.data);
+        setLoading(false);
+      })
+      .catch((err) => {
+        console.log(err);
       });
-    setLoading(true);
-    // Use this data to update the state
-  };
-
-  useEffect(() => {
-    getList();
   }, []);
-
-  const filterList = (text) => {
-    let filteredList = artist.filter(function (item) {
-      return item.title.toLowerCase().search(text.toLowerCase()) !== -1;
-    });
-    setArtist(filteredList);
-  };
+  
+  useEffect(() => {
+    artist && setFilteredArtists(
+      artist.filter((artist) =>
+      artist.title.toLowerCase().includes(search.toLowerCase())
+      )
+    );
+  }, [search, artist]);
 
   return (
     <div>
+      {!loading ? ( 
+    <div>
       <h2>Artists</h2>
-      <SearchPlugin filter={filterList} />
+      <input
+        type="text"
+        placeholder="Search"
+        onChange={(e) => setSearch(e.target.value.trim())}
+      />
       <ul>
-        {loading ? (
-          artist.map((artist) => <ArtistCard key={artist.id} artist={artist} />)
-        ) : (
-          <LoadingSpinner />
-        )}
+        {filteredArtists.map((artist) => <ArtistCard key={artist.id} artist={artist} />)}
       </ul>
+    </div>
+    ) : (
+      <LoadingSpinner />
+    )}
     </div>
   );
 }
