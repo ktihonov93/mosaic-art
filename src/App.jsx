16421b39845033f2a57197ce0456a20846c67dff
k_tihonov <k_tihonov@mail.ru>
2021-05-26 20:41:17 +0300

    fix description in home.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ import Footer from "./components/Footer"
 function App() {
   return (
     <Router>
-      <div>        
+      <div className="content">        
         <MainNavbar />
         
         <Switch>
